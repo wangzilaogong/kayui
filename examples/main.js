7d8def9a3b2e109f0a530b0c86c528ebc54d47b7
wk <997972547@qq.com>
2019-01-31 11:48:55 +0800

    3.0 init

--- examples/main.js
+++ examples/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import kUi from '../packages/index'
+
+Vue.use(kUi)
+
 Vue.config.productionTip = false
 
 new Vue({
